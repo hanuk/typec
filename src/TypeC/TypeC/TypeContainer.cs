f3ae29603dfc2b839cdbda83bd2c945df48aedc7
Hanu Kommalapati <hanuk@live.com>
2015-02-27 17:48:50 -0600

    added source code comments in TypeContainer

--- src/TypeC/TypeC/TypeContainer.cs
+++ src/TypeC/TypeC/TypeContainer.cs
@@ -19,29 +19,54 @@ using System.Threading.Tasks;
 namespace TypeC
 {
 	/// <summary>
-	/// simple factory class that 
+	/// simple factory class that manage type registration and dependency resolution. This doesn't do any dependency injection. 
 	/// </summary>
 	public class TypeContainer
 	{
+		/// <summary>
+		/// Type registration without namespace goes into DEFAULT namespace
+		/// </summary>
 		private static readonly string DEFAULT_NAMESPACE = "DEFAULT";
+		/// <summary>
+		/// Singleton instance
+		/// </summary>
 		private static TypeContainer _instance = new TypeContainer();
+		/// <summary>
+		/// Registered types are stored in nested dictionaries 
+		/// </summary>
 		private Dictionary<string, Dictionary<Type, Type>> _typeRegistry;
 
+		/// <summary>
+		/// static reference that is part of the singleton implementation
+		/// </summary>
 		public static TypeContainer Instance
 		{
 			get { return _instance; }
 		}
 
+		/// <summary>
+		/// private constructor for singleton implementation
+		/// </summary>
 		private TypeContainer()
 		{
 			_typeRegistry = new Dictionary<string, Dictionary<Type, Type>>();
 		}
+		/// <summary>
+		/// Registers .NET Type objects into DEFAULT namespace
+		/// </summary>
+		/// <typeparam name="TFromType">The abstract Type</typeparam>
+		/// <typeparam name="TToType">The concrete Type</typeparam>
 		public void Register<TFromType, TToType>()
 		{
 			string nameSpace = DEFAULT_NAMESPACE;
 			Register<TFromType, TToType>(nameSpace);
-
 		}
+		/// <summary>
+		/// Registers .NET Type objects into the given namespace
+		/// </summary>
+		/// <typeparam name="TFromType">The abstract Type</typeparam>
+		/// <typeparam name="TToType">The concrete Type</typeparam>
+		/// <param name="nameSpace"></param>
 		public void Register<TFromType, TToType>(string nameSpace)
 		{
 			if (!_typeRegistry.ContainsKey(nameSpace))
@@ -60,6 +85,12 @@ namespace TypeC
 				registry.Add(from, to);
 			}
 		}
+		/// <summary>
+		/// Creates instance of a .NET Type registered in a given namespace
+		/// </summary>
+		/// <typeparam name="TFromType">The abstract Type</typeparam>
+		/// <typeparam name="TToType">The concrete Type</typeparam>
+		/// <returns>Null the object or Null if not found</returns>
 		public TFromType GetInstance<TFromType>(string nameSpace) where TFromType : class
 		{
 			if (_typeRegistry.ContainsKey(nameSpace))
@@ -72,7 +103,12 @@ namespace TypeC
 			}
 		}
 
-
+		/// <summary>
+		/// Utility method for Type lookup and instantiation from a given type registry
+		/// </summary>
+		/// <typeparam name="TFromType">The abstract Type</typeparam>
+		/// <typeparam name="TToType">The concrete Type</typeparam>
+		/// <returns>Null the object or null if not found</returns>
 		private TFromType GetInstance<TFromType>(Dictionary<Type, Type> typeRegistry) where TFromType : class
 		{
 			Type from = typeof(TFromType);
@@ -86,7 +122,12 @@ namespace TypeC
 				return null;
 			}
 		}
-
+		/// <summary>
+		/// Creates instance of a .NET Type registered in a DEFAULT namespace
+		/// </summary>
+		/// <typeparam name="TFromType">The abstract Type</typeparam>
+		/// <typeparam name="TToType">The concrete Type</typeparam>
+		/// <returns>Null the object or null if not found</returns>
 		public TFromType GetInstance<TFromType>() where TFromType : class
 		{
 			string nameSpace = DEFAULT_NAMESPACE;
